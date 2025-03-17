c4399cdd38d57d336c2339a142a800a51906846a
swjang128 <swjang128@gmail.com>
2025-03-17 17:26:37 +0900

    문제가 되는 테이블 엔티티 제외(추후 추가하기)

--- core/src/main/java/com/redutec/core/repository/CmtConfigurationDiagnosisRAQRangeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.redutec.core.repository;
-
-import com.redutec.core.entity.CmtConfigurationDiagnosisRAQRange;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CmtConfigurationDiagnosisRAQRangeRepository extends JpaRepository<CmtConfigurationDiagnosisRAQRange, String>, JpaSpecificationExecutor<CmtConfigurationDiagnosisRAQRange> {
-}
