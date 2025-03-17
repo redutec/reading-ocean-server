bed88c0eab261f7f8eb6230c3aaba59119f53c09
swjang128 <swjang128@gmail.com>
2025-03-18 08:36:08 +0900

    관리할 필요없는 엔티티 제거

--- core/src/main/java/com/redutec/core/repository/CmtProcessLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.redutec.core.repository;
-
-import com.redutec.core.entity.CmtProcessLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CmtProcessLogRepository extends JpaRepository<CmtProcessLog, Integer>, JpaSpecificationExecutor<CmtProcessLog> {
-}
